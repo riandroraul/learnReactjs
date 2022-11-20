190dcdbfef68dcb8e22f5b5794faae02c87e8bc9
riandroraul <riandroraul@gmail.com>
2022-11-20 20:18:51 +0700

    update

---
+++ my-app/src/components/Home/image.jsx
@@ -0,0 +1,8 @@
+import imgJS from "../../Assets/img/Youtube/JavaScript.png";
+import imgPY from "../../Assets/img/Youtube/python.png";
+import imgMongo from "../../Assets/img/Youtube/MongoDB.jpg";
+import imgNodejs from "../../Assets/img/Youtube/node-js.png";
+import imgReact from "../../Assets/img/Youtube/reactjs.png";
+import imgRest from "../../Assets/img/Youtube/rest-api.jpg";
+
+export { imgJS, imgPY, imgMongo, imgNodejs, imgReact, imgRest };
