61d143d31da98d5b13a9dbcce57299ada2df3043
riandroraul <riandroraul@gmail.com>
2022-11-22 20:20:28 +0700

    update

--- my-app/src/components/Home/Home.jsx
+++ my-app/src/components/Home/Home.jsx
@@ -1,64 +1,35 @@
-import React from "react";
-import Youtube from "../Youtube/Youtube";
-import { imgJS, imgPY, imgMongo, imgNodejs, imgReact, imgRest } from "./image";
-import {
-  BrowserRouter as Router,
-  createBrowserRouter,
-  Route,
-  Link,
-} from "react-router-dom";
+import React, { Fragment } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import BlogPost from "../BlogPost";
+import Product from "../Product/Product";
+import YoutubeWrapper from "../Youtube";
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <BlogPost />,
+    },
+    {
+      path: "/youtube",
+      element: <YoutubeWrapper />,
+    },
+    {
+      path: "/product",
+      element: <Product />,
+    },
+  ]
+  // createRoutesFromElements(
+  //   <Route path="/youtube" element={<YoutubeWrapper />} />
+  // )
+);
 
 const Home = () => {
   return (
-    <Router>
-      <div>
-        <h4>Blog Post</h4>
-        <hr />
-        <Route path="/">
-          <BlogPost />
-        </Route>
-        <Route path="/youtube">
-          <Youtube
-            image={imgJS}
-            time="12:42"
-            title="Belajar Javascript"
-            desc="tutorial javascript untuk pemula"
-          />
-          <Youtube
-            image={imgPY}
-            time="14:30"
-            title="Belajar Python"
-            desc="tutorial python untuk pemula"
-          />
-          <Youtube
-            image={imgMongo}
-            time="10:31"
-            title="Belajar MongoDB"
-            desc="tutorial MongoDB untuk pemula"
-          />
-          <Youtube
-            image={imgNodejs}
-            time="17:38"
-            title="Belajar Node Js"
-            desc="tutorial Node Js untuk pemula"
-          />
-          <Youtube
-            image={imgReact}
-            time="27:09"
-            title="Belajar React Js"
-            desc="tutorial React Js untuk pemula"
-          />
-          <Youtube
-            image={imgRest}
-            time="30:05"
-            title="Belajar REST API"
-            desc="tutorial Rest API untuk pemula"
-          />
-        </Route>
-      </div>
-    </Router>
+    <Fragment>
+      <RouterProvider router={router} />
+    </Fragment>
   );
 };
 
--- my-app/src/components/Product/CardProduct.jsx
+++ my-app/src/components/Product/CardProduct.jsx
@@ -1,48 +1,48 @@
-import React, { Component } from 'react';
-import keyboard from '../../Assets/img/Product/keyboard.jpg'
+import React, { Component } from "react";
+import keyboard from "../../Assets/img/Product/keyboard.jpg";
 
-
-class CardProduct extends Component{
-
+class CardProduct extends Component {
   state = {
     order: 4,
-    name: 'rian'
-  }
-  
+    name: "rian",
+  };
+
   handleCounterChange = (newValue) => {
-   this.props.onCounterChange(newValue)
-  }
+    this.props.onCounterChange(newValue);
+  };
 
   btnPlus = () => {
-   this.setState({order: this.state.order + 1}, 
-    () => {
-      this.handleCounterChange(this.state.order)
-    })
-  }
+    this.setState({ order: this.state.order + 1 }, () => {
+      this.handleCounterChange(this.state.order);
+    });
+  };
 
- btnMinus = () => {
-   if(this.state.order > 0){
-     this.setState({order: this.state.order - 1}, 
-      () => this.handleCounterChange(this.state.order))
-   }
- }
-  render(){
-
-    
+  btnMinus = () => {
+    if (this.state.order > 0) {
+      this.setState({ order: this.state.order - 1 }, () =>
+        this.handleCounterChange(this.state.order)
+      );
+    }
+  };
+  render() {
     return (
-      <div className='card'>
-          <div className="img-thumb-prod">
-            <img src={keyboard} alt="" />
-          </div>
-          <p className="product-title">Keyboard Gaming</p>
-          <p className="product-price">Rp. 30.000</p>
-          <div className="counter">
-            <button className="minus" onClick={this.btnMinus}>-</button>
-            <input type="text" value={this.state.order} />
-            <button className="plus" onClick={this.btnPlus}>+</button>
-          </div>
+      <div className="card">
+        <div className="img-thumb-prod">
+          <img src={keyboard} alt="" />
         </div>
-    )
+        <p className="product-title">Keyboard Gaming</p>
+        <p className="product-price">Rp. 200.000</p>
+        <div className="counter">
+          <button className="minus" onClick={this.btnMinus}>
+            -
+          </button>
+          <input type="text" value={this.state.order} />
+          <button className="plus" onClick={this.btnPlus}>
+            +
+          </button>
+        </div>
+      </div>
+    );
   }
 }
 
