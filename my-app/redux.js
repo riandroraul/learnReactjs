61a45e1f43e23428f428c7ed6b37855cabe815f1
riandroraul <riandroraul@gmail.com>
2022-12-02 16:25:54 +0700

    update

--- my-app/redux.js
+++ my-app/redux.js
@@ -1,3 +1,34 @@
-import redux from "redux";
+const redux = require("redux");
 
 const createStore = redux.createStore;
+
+const initialState = {
+  value: 0,
+  age: 5,
+};
+
+// Reducer
+const rootReducer = (state = initialState, action) => {
+  console.log(action.type);
+  switch (action.type) {
+    case "ADD_AGE":
+      return {
+        ...state,
+        age: state.age + 1,
+      };
+      break;
+    default:
+      return state;
+      break;
+  }
+};
+
+// store
+const store = createStore(rootReducer);
+console.log(store.getState());
+
+// dispatching action
+store.dispatch({ type: "ADD_AGE" });
+console.log(store.getState());
+
+// subscription
